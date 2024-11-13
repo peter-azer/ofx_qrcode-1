76289622b3823da347b7cac63f7e17dd000c598a
moofx19 <mahmoudofx@gmail.com>
2024-11-13 14:51:57 +0200

    Commit message for your changes

--- app/Notifications/ResetPasswordNotification.php
+++ app/Notifications/ResetPasswordNotification.php
@@ -12,42 +12,32 @@ use Illuminate\Support\Facades\URL;
 /**
  * Create a new notification instance.
  */
-class ResetPasswordNotification extends Notification
-{
 
-
-    use Queueable;
-    public $token;
-    public $email;
-
-    public function __construct($token, $email)
-    {
-        $this->token = $token;
-        $this->email = $email;
-    }
-
-    public function via($notifiable)
-    {
-        return ['mail'];
-    }
-
-    public function toMail($notifiable)
-    {
-        // Custom reset URL with your desired base URL
-        $resetUrl = "https://ofx-qrcode.com/resetpassword?token={$this->token}&email={$this->email}";
-
-  
-
-        return (new MailMessage)
-        ->subject('Password Reset Request')  // You can change the subject here
-        ->line('You are receiving this email because we received a password reset request for your account.')
-        // Remove the default logo and home link by not including this
-     
-        ->line('Click the button below to reset your password:')
-        // Set your custom logo URL here
+ class ResetPasswordNotification extends Notification
+ {
+     protected $token;
+     protected $email;
  
-        ->action('Reset Password', $resetUrl)
-        ->line('If you did not request a password reset, no further action is required.')
-        ->line('Thanks, OFXQRCode');
-}
-}
+     public function __construct($token, $email)
+     {
+         $this->token = $token;
+         $this->email = $email;
+     }
+ 
+     public function toMail($notifiable)
+     {
+         // Custom reset URL with your desired base URL
+         $resetUrl = "https://ofx-qrcode.com/resetpassword?token={$this->token}&email={$this->email}";
+ 
+         return (new MailMessage)
+             ->subject('Password Reset Request')  // Custom subject
+             ->line('You are receiving this email because we received a password reset request for your account.')
+             // Custom logo URL and removing default Laravel logo by not including it
+             ->line("<img src='https://backend.ofx-qrcode.com/storage/ofxqr-logo/OFX-QR%20logo.png' alt='OFX QR Code Logo' style='max-width: 200px; height: auto; margin-bottom: 20px;' />")
+             ->line('Click the button below to reset your password:')
+             ->action('Reset Password', $resetUrl)
+             ->line('If you did not request a password reset, no further action is required.')
+             ->line('Thanks, OFXQRCode');
+     }
+ }
+
