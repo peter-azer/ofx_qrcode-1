1a53c17e5e4974ac2e8ae068df802c3bb54390cc
moofx19 <mahmoudofx@gmail.com>
2024-11-13 14:38:41 +0200

    Commit message for your changes

--- app/Notifications/ResetPasswordNotification.php
+++ app/Notifications/ResetPasswordNotification.php
@@ -42,10 +42,10 @@ class ResetPasswordNotification extends Notification
         ->subject('Password Reset Request')  // You can change the subject here
         ->line('You are receiving this email because we received a password reset request for your account.')
         // Remove the default logo and home link by not including this
-        ->greeting('Hello!')
+     
         ->line('Click the button below to reset your password:')
         // Set your custom logo URL here
-        ->line('<img src="https://backend.ofx-qrcode.com/storage/ofxqr-logo/OFX-QR%20logo.png" alt="OFX QR Logo" style="max-width: 200px; height: auto;">')
+ 
         ->action('Reset Password', $resetUrl)
         ->line('If you did not request a password reset, no further action is required.')
         ->line('Thanks, OFXQRCode');
