98a6718cd4ce89b6526890c4396a0fd7bdb72bc7
mostafafayez <fayezmostafa51@gmail.com>
2024-11-14 11:15:32 +0200

    first commit

--- app/Services/GeideaPaymentService.php
+++ app/Services/GeideaPaymentService.php
@@ -8,7 +8,6 @@ use GuzzleHttp\Client;
 class GeideaPaymentService
 {
 
-
     protected $baseUrl;
     protected $publicKey;
     protected $apiPassword;
@@ -16,14 +15,14 @@ class GeideaPaymentService
     public function __construct()
     {
         $this->baseUrl = 'https://api.merchant.geidea.net';
-        $this->publicKey = config('services.geidea.public_key');
-        $this->apiPassword = config('services.geidea.api_password');
+        $this->publicKey = env('GEIDEA_PUBLIC_KEY');
+        $this->apiPassword = env('GEIDEA_API_PASSWORD');
     }
 
     public function initiatePayment($amount, $currency, $orderId, $callbackUrl)
     {
         $response = Http::withBasicAuth($this->publicKey, $this->apiPassword)
-            ->post("{$this->baseUrl}/v2/checkout", [
+            ->post("{$this->baseUrl}/v2", [
                 'amount' => $amount,
                 'currency' => $currency,
                 'orderId' => $orderId,
