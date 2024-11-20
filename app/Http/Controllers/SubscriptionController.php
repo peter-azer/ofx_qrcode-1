53b875d3f4ad329ee3e6fa81624df07309c90474
mostafafayez <fayezmostafa51@gmail.com>
2024-11-20 18:09:01 +0200

    first commit

--- app/Http/Controllers/SubscriptionController.php
+++ app/Http/Controllers/SubscriptionController.php
@@ -112,6 +112,7 @@ public function updateQrCodeLimit(Request $request)
 
     // Update the qrcode_limit in the pivot table
     $newQrCodeLimit = $userPackage->pivot->qrcode_limit + $validatedData['qrcode_limit'];
+    $userPackage->pivot->qrcode_limit =$newQrCodeLimit;
     $userPackage->pivot->is_enable='1';
     $userPackage->pivot->save();
    QrCodeModel::where('user_id', $user->id)->update(['is_active' => 1]);
