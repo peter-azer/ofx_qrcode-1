efcc1d0a9f027050860e44a919e3b9d764e198bd
mostafafayez <fayezmostafa51@gmail.com>
2024-11-20 17:43:41 +0200

    first commit

--- app/Http/Controllers/Smart_QRCodeController.php
+++ app/Http/Controllers/Smart_QRCodeController.php
@@ -223,184 +223,184 @@ class Smart_QRCodeController extends Controller
 //////////////////////////////
 
 
-public function generatesmartQRCode(Request $request)
-{
-    DB::beginTransaction();
+// public function generatesmartQRCode(Request $request)
+// {
+//     DB::beginTransaction();
 
-    try {
-        $user = $request->user();
+//     try {
+//         $user = $request->user();
 
-        $validatedData = $request->validate([
-            'title' => 'nullable|string',
-            'description' => 'nullable|string',
-            'phones' => 'nullable|array',
-            'logo' => 'nullable|file|mimes:jpeg,png,jpg',
-            'cover' => 'nullable|file|mimes:jpeg,png,jpg',
-            'color' => 'nullable|string',
-            'font' => 'nullable|string',
-            'package_id' => 'nullable|string',
-            'links' => 'nullable|array',
-            'links.*.url' => 'nullable|string',
-            'links.*.type' => 'nullable|string',
-            'images' => 'required|array',
-            'images.*' => 'nullable|file|mimes:jpeg,png,jpg',
-            'mp3' => 'nullable|array',
-            'mp3.*' => 'nullable|file',
-            'pdfs' => 'nullable|array',
-            'pdfs.*' => 'nullable|file|mimes:pdf',
-            'event_date' => 'nullable',
-            'event_time' => 'nullable',
-            'location' => 'nullable|string',
-            'branches' => 'nullable|array',
-            'branches.*.name' => 'required|string',
-            'branches.*.location' => 'required|string',
-            'branches.*.phones' => 'nullable|array',
-        ]);
+//         $validatedData = $request->validate([
+//             'title' => 'nullable|string',
+//             'description' => 'nullable|string',
+//             'phones' => 'nullable|array',
+//             'logo' => 'nullable|file|mimes:jpeg,png,jpg',
+//             'cover' => 'nullable|file|mimes:jpeg,png,jpg',
+//             'color' => 'nullable|string',
+//             'font' => 'nullable|string',
+//             'package_id' => 'nullable|string',
+//             'links' => 'nullable|array',
+//             'links.*.url' => 'nullable|string',
+//             'links.*.type' => 'nullable|string',
+//             'images' => 'required|array',
+//             'images.*' => 'nullable|file|mimes:jpeg,png,jpg',
+//             'mp3' => 'nullable|array',
+//             'mp3.*' => 'nullable|file',
+//             'pdfs' => 'nullable|array',
+//             'pdfs.*' => 'nullable|file|mimes:pdf',
+//             'event_date' => 'nullable',
+//             'event_time' => 'nullable',
+//             'location' => 'nullable|string',
+//             'branches' => 'nullable|array',
+//             'branches.*.name' => 'required|string',
+//             'branches.*.location' => 'required|string',
+//             'branches.*.phones' => 'nullable|array',
+//         ]);
 
-        $profile = Profile::create([
-            'user_id' => $user->id,
-            'logo' => $request->file('logo') ? $request->file('logo')->store('logos', 'public') : null,
-            'phones' => $validatedData['phones'] ?? null,
-            'cover' => $request->file('cover') ? $request->file('cover')->store('covers', 'public') : null,
-            'background_color' => $validatedData['color'] ?? null,
-            'title' => $validatedData['title'] ?? null,
-            'description' => $validatedData['description'] ?? null,
-            'font' => $validatedData['font'] ?? null,
-        ]);
+//         $profile = Profile::create([
+//             'user_id' => $user->id,
+//             'logo' => $request->file('logo') ? $request->file('logo')->store('logos', 'public') : null,
+//             'phones' => $validatedData['phones'] ?? null,
+//             'cover' => $request->file('cover') ? $request->file('cover')->store('covers', 'public') : null,
+//             'background_color' => $validatedData['color'] ?? null,
+//             'title' => $validatedData['title'] ?? null,
+//             'description' => $validatedData['description'] ?? null,
+//             'font' => $validatedData['font'] ?? null,
+//         ]);
 
-        // Batch insert links
-        if (!empty($validatedData['links'])) {
-            $linkData = array_map(fn($link) => [
-                'profile_id' => $profile->id,
-                'url' => $link['url'],
-                'type' => $link['type']
-            ], array_filter($validatedData['links'], fn($link) => !empty($link['url']) && !empty($link['type'])));
-            links::insert($linkData);
-        }
+//         // Batch insert links
+//         if (!empty($validatedData['links'])) {
+//             $linkData = array_map(fn($link) => [
+//                 'profile_id' => $profile->id,
+//                 'url' => $link['url'],
+//                 'type' => $link['type']
+//             ], array_filter($validatedData['links'], fn($link) => !empty($link['url']) && !empty($link['type'])));
+//             links::insert($linkData);
+//         }
 
-        // Batch insert branches
-        if (!empty($validatedData['branches'])) {
-            foreach ($validatedData['branches'] as $branchData) {
-                branches::create([
-                    'profile_id' => $profile->id,
-                    'name' => $branchData['name'],
-                    'location' => $branchData['location'],
-                    'phones' => isset($branchData['phones']) ? json_encode($branchData['phones']) : null, // Convert array to JSON
-                ]);
-            }
-        }
+//         // Batch insert branches
+//         if (!empty($validatedData['branches'])) {
+//             foreach ($validatedData['branches'] as $branchData) {
+//                 branches::create([
+//                     'profile_id' => $profile->id,
+//                     'name' => $branchData['name'],
+//                     'location' => $branchData['location'],
+//                     'phones' => isset($branchData['phones']) ? json_encode($branchData['phones']) : null, // Convert array to JSON
+//                 ]);
+//             }
+//         }
 
 
-// Initialize file paths arrays
-$imagePaths = [];
-$pdfPaths = [];
-$mp3Paths = [];
+// // Initialize file paths arrays
+// $imagePaths = [];
+// $pdfPaths = [];
+// $mp3Paths = [];
 
-// Process and save images
-if ($request->hasFile('images')) {
-    foreach ($request->file('images') as $image) {
-        $path = $image->store('images', 'public');
-        $imagePaths[] = [
-            'profile_id' => $profile->id,
-            'image_path' => $path,
-        ];
-    }
-}
+// // Process and save images
+// if ($request->hasFile('images')) {
+//     foreach ($request->file('images') as $image) {
+//         $path = $image->store('images', 'public');
+//         $imagePaths[] = [
+//             'profile_id' => $profile->id,
+//             'image_path' => $path,
+//         ];
+//     }
+// }
 
-// Process and save PDFs
-if ($request->hasFile('pdfs')) {
-    foreach ($request->file('pdfs') as $pdf) {
-        $path = $pdf->store('pdfs', 'public');
-        $pdfPaths[] = [
-            'profile_id' => $profile->id,
-            'pdf_path' => $path,
-        ];
-    }
-}
+// // Process and save PDFs
+// if ($request->hasFile('pdfs')) {
+//     foreach ($request->file('pdfs') as $pdf) {
+//         $path = $pdf->store('pdfs', 'public');
+//         $pdfPaths[] = [
+//             'profile_id' => $profile->id,
+//             'pdf_path' => $path,
+//         ];
+//     }
+// }
 
-// Process and save MP3s
-if ($request->hasFile('mp3')) {
-    foreach ($request->file('mp3') as $mp3) {
-        $path = $mp3->store('records', 'public');
-        $mp3Paths[] = [
-            'profile_id' => $profile->id,
-            'mp3_path' => $path,
-        ];
-    }
-}
+// // Process and save MP3s
+// if ($request->hasFile('mp3')) {
+//     foreach ($request->file('mp3') as $mp3) {
+//         $path = $mp3->store('records', 'public');
+//         $mp3Paths[] = [
+//             'profile_id' => $profile->id,
+//             'mp3_path' => $path,
+//         ];
+//     }
+// }
 
-// Queue file processing using only paths
-Queue::push(function () use ($imagePaths, $pdfPaths, $mp3Paths) {
-    Log::info('Queue job started for file uploads');
-    if (!empty($imagePaths)) {
-        images::insert($imagePaths);
-        dd('Images saved:', $imagePaths);
-    }
+// // Queue file processing using only paths
+// Queue::push(function () use ($imagePaths, $pdfPaths, $mp3Paths) {
+//     Log::info('Queue job started for file uploads');
+//     if (!empty($imagePaths)) {
+//         images::insert($imagePaths);
+//         dd('Images saved:', $imagePaths);
+//     }
 
-    if (!empty($pdfPaths)) {
-        Pdfs::insert($pdfPaths);
-    }
+//     if (!empty($pdfPaths)) {
+//         Pdfs::insert($pdfPaths);
+//     }
 
-    if (!empty($mp3Paths)) {
-        records::insert($mp3Paths);
-    }
-});
+//     if (!empty($mp3Paths)) {
+//         records::insert($mp3Paths);
+//     }
+// });
 
-        // Insert event if provided
-        if (!empty($validatedData['event_date'])) {
-            events::create([
-                'profile_id' => $profile->id,
-                'event_date' => $validatedData['event_date'],
-                'event_time' => $validatedData['event_time'],
-                'location' => $validatedData['location']
-            ]);
-        }
+//         // Insert event if provided
+//         if (!empty($validatedData['event_date'])) {
+//             events::create([
+//                 'profile_id' => $profile->id,
+//                 'event_date' => $validatedData['event_date'],
+//                 'event_time' => $validatedData['event_time'],
+//                 'location' => $validatedData['location']
+//             ]);
+//         }
 
-        // Generate QR code data
-        $uniqueName = uniqid();
-        $qrCodeLink = 'https://ofx-qrcode.com/qr/' . $uniqueName;
+//         // Generate QR code data
+//         $uniqueName = uniqid();
+//         $qrCodeLink = 'https://ofx-qrcode.com/qr/' . $uniqueName;
 
-        $qrCodeData = QrCode::format('png')
-            ->backgroundColor(255, 255, 255)
-            ->size(200)
-            ->color(0, 0, 0)
-            ->generate($qrCodeLink);
+//         $qrCodeData = QrCode::format('png')
+//             ->backgroundColor(255, 255, 255)
+//             ->size(200)
+//             ->color(0, 0, 0)
+//             ->generate($qrCodeLink);
 
-        $fileName = 'qrcodes/' . uniqid() . '.png';
-        Storage::disk('public')->put($fileName, $qrCodeData);
+//         $fileName = 'qrcodes/' . uniqid() . '.png';
+//         Storage::disk('public')->put($fileName, $qrCodeData);
 
-        // Create QR code record
-        $qrCode = QrCodeModel::create([
-            'profile_id' => $profile->id,
-            'user_id' => $user->id,
-            'qrcode' => $fileName,
-            'link' => $qrCodeLink,
-            'package_id' => $validatedData['package_id'] ?? null,
-            'scan_count' => 0,
-            'is_active' => true,
-        ]);
+//         // Create QR code record
+//         $qrCode = QrCodeModel::create([
+//             'profile_id' => $profile->id,
+//             'user_id' => $user->id,
+//             'qrcode' => $fileName,
+//             'link' => $qrCodeLink,
+//             'package_id' => $validatedData['package_id'] ?? null,
+//             'scan_count' => 0,
+//             'is_active' => true,
+//         ]);
 
-        DB::commit();
+//         DB::commit();
 
-        return response()->json([
-            'message' => 'QR code generated successfully',
-            'qr_code' => $qrCode->qrcode,
-            'link' => $qrCode->link
-        ], 200);
-    } catch (ValidationException $e) {
-        DB::rollBack();
-        return response()->json([
-            'message' => 'Validation errors occurred.',
-            'errors' => $e->validator->errors()
-        ], 422);
-    } catch (\Exception $e) {
-        DB::rollBack();
-        return response()->json([
-            'message' => 'An error occurred while generating the QR code.',
-            'error' => $e->getMessage()
-        ], 500);
-    }
-}
+//         return response()->json([
+//             'message' => 'QR code generated successfully',
+//             'qr_code' => $qrCode->qrcode,
+//             'link' => $qrCode->link
+//         ], 200);
+//     } catch (ValidationException $e) {
+//         DB::rollBack();
+//         return response()->json([
+//             'message' => 'Validation errors occurred.',
+//             'errors' => $e->validator->errors()
+//         ], 422);
+//     } catch (\Exception $e) {
+//         DB::rollBack();
+//         return response()->json([
+//             'message' => 'An error occurred while generating the QR code.',
+//             'error' => $e->getMessage()
+//         ], 500);
+//     }
+// }
 
 
 
