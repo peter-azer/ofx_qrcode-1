c0912e9b0dd976c1483bd3ad1bff46d2596dd659
mostafafayez <fayezmostafa51@gmail.com>
2024-11-21 10:11:18 +0200

    first commit

--- app/Console/Commands/NotifyExpiringSubscriptions.php
+++ app/Console/Commands/NotifyExpiringSubscriptions.php
@@ -29,11 +29,19 @@ class NotifyExpiringSubscriptions extends Command
         })->get();
 
         foreach ($users as $user) {
-            // Send email
-            Mail::raw("Your subscription is ending soon!", function ($message) use ($user) {
-                $message->to($user->email)
-                        ->subject('Subscription Expiry Notification');
-            });
+            try {
+                // Send email
+                Mail::raw("Your subscription is ending soon!", function ($message) use ($user) {
+                    $message->to($user->email)
+                            ->subject('Subscription Expiry Notification');
+                });
+
+                // Log email
+                \Log::info('Subscription expiry notification sent to: ' . $user->email);
+            } catch (\Exception $e) {
+                // Log error if email fails
+                \Log::error('Failed to send email to: ' . $user->email . ' | Error: ' . $e->getMessage());
+            }
         }
 
         $this->info('Notifications sent to users with expiring subscriptions.');
