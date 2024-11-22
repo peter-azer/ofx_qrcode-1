b186f0abff9121a7b3fe7707f06897abba18a040
mostafafayez <fayezmostafa51@gmail.com>
2024-11-22 15:45:53 +0200

    first commit

--- app/Mail/SubscriptionReminder.php
+++ app/Mail/SubscriptionReminder.php
@@ -31,14 +31,14 @@ class SubscriptionReminder extends Mailable
      */
     public function build()
     {
-        $endDate = Carbon::parse($this->user->packages->first()->pivot->end_date);
+        // $endDate = Carbon::parse($this->user->packages->first()->pivot->end_date);
 
         return $this->subject('Subscription Expiry Reminder')
                     ->view('emails.subscription_reminder')
                     ->with([
                         'name' => $this->user->name,
                         'email' => $this->user->email,
-                        'endDate' => $endDate, // Pass Carbon instance here
+
                     ]);
     }
 }
