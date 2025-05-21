29eea2b74cbe3b8b36f874a09e0a9b1b2b671646
moofx19 <mahmoudofx@gmail.com>
2025-05-21 16:08:04 +0300

    Refactor GeideaPaymentService for improved code readability and consistency; update publicKey and apiPassword initialization

--- app/Services/GeideaPaymentService.php
+++ app/Services/GeideaPaymentService.php
@@ -5,6 +5,7 @@ namespace App\Services;
 use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Str;
+
 class GeideaPaymentService
 {
     protected $baseUrl;
@@ -17,14 +18,14 @@ class GeideaPaymentService
         $this->baseUrl = 'https://api.merchant.geidea.net/payment-intent/api/v2/direct/session'; // Correct URL for payment session
         // $this->publicKey = 'c940b85f-c8f7-4229-a853-7c44d4a8db2f'; //test
         // $this->apiPassword ='225235e9-336a-45aa-91b4-ff9cfd31be50';
- $this->publicKey = 'b42f3fd5-782e-4d86-afda-0efbed7a1711'; //live
-$this->apiPassword ='924eedea-4be1-4ef5-8324-e42e23255337';
+        $this->publicKey = 'c940b85f-c8f7-4229-a853-7c44d4a8db2f'; //live
+        $this->apiPassword = '9cde412c-74e7-47ea-96e9-66f847bf4198';
     }
 
     /**
      * Generate the signature using the provided data.
      */
-    public function generateSignature($merchantPublicKey, $amount, $orderCurrency, $orderMerchantReferenceId,$apiPassword, $timestamp)
+    public function generateSignature($merchantPublicKey, $amount, $orderCurrency, $orderMerchantReferenceId, $apiPassword, $timestamp)
     {
 
         $amountStr = number_format($amount, 2, '.', '');
@@ -32,11 +33,11 @@ $this->apiPassword ='924eedea-4be1-4ef5-8324-e42e23255337';
         $data = "{$merchantPublicKey}{$amountStr}{$orderCurrency}{$orderMerchantReferenceId}{$timestamp}";
 
         // Generate HMAC SHA256 hash and encode it in Base64
-        $hash = hash_hmac('sha256', $data,  $this->apiPassword,true);
+        $hash = hash_hmac('sha256', $data,  $this->apiPassword, true);
         return base64_encode($hash);
     }
 
-  /**
+    /**
 
      *
      * @param double $amount -
@@ -46,52 +47,48 @@ $this->apiPassword ='924eedea-4be1-4ef5-8324-e42e23255337';
      *
      */
 
-     public function createSession($amount, $currency, $callbackUrl)
-{
+    public function createSession($amount, $currency, $callbackUrl)
+    {
 
-    $formattedAmount = number_format($amount, 2, '.', '');
-
-
-    $merchantReferenceId = uniqid();
-    $timestamp = date('Y-m-d\TH:i:s\Z');
-    $signature = $this->generateSignature('b42f3fd5-782e-4d86-afda-0efbed7a1711', $formattedAmount, $currency, $merchantReferenceId,'924eedea-4be1-4ef5-8324-e42e23255337', $timestamp);
-
-    // Prepare the payload for the API request
-    $payload = [
-        'amount' => $formattedAmount,
-        'currency' => $currency,
-        'timestamp' => $timestamp,
-        'merchantReferenceId' => $merchantReferenceId,
-        'signature' =>$signature,
-        'callbackUrl' => $callbackUrl,
-    ];
+        $formattedAmount = number_format($amount, 2, '.', '');
 
 
+        $merchantReferenceId = uniqid();
+        $timestamp = date('Y-m-d\TH:i:s\Z');
+        $signature = $this->generateSignature('b42f3fd5-782e-4d86-afda-0efbed7a1711', $formattedAmount, $currency, $merchantReferenceId, '924eedea-4be1-4ef5-8324-e42e23255337', $timestamp);
 
-    // Log::info(' Request:', ['Request' => $payload]);
+        // Prepare the payload for the API request
+        $payload = [
+            'amount' => $formattedAmount,
+            'currency' => $currency,
+            'timestamp' => $timestamp,
+            'merchantReferenceId' => $merchantReferenceId,
+            'signature' => $signature,
+            'callbackUrl' => $callbackUrl,
+        ];
 
-    try {
 
 
-        $response = Http::withBasicAuth($this->publicKey, $this->apiPassword)
-            ->post($this->baseUrl, $payload);
+        // Log::info(' Request:', ['Request' => $payload]);
 
-        if ($response->successful()) {
-            Log::info('Geidea Payment Session Response:', ['response' => $response->json()]);
-            return $response->json();
-        } else {
+        try {
 
-            Log::error('Geidea API Error: ', ['error' => $response->body()]);
-            return ['error' => 'Failed to initiate payment session', 'details' => $response->body()];
+
+            $response = Http::withBasicAuth($this->publicKey, $this->apiPassword)
+                ->post($this->baseUrl, $payload);
+
+            if ($response->successful()) {
+                Log::info('Geidea Payment Session Response:', ['response' => $response->json()]);
+                return $response->json();
+            } else {
+
+                Log::error('Geidea API Error: ', ['error' => $response->body()]);
+                return ['error' => 'Failed to initiate payment session', 'details' => $response->body()];
+            }
+        } catch (\Exception $e) {
+
+            Log::error('Geidea API Exception: ', ['exception' => $e->getMessage()]);
+            return ['error' => 'Error initiating payment session', 'exception' => $e->getMessage()];
         }
-    } catch (\Exception $e) {
-
-        Log::error('Geidea API Exception: ', ['exception' => $e->getMessage()]);
-        return ['error' => 'Error initiating payment session', 'exception' => $e->getMessage()];
     }
 }
-
-
-
-    }
-
