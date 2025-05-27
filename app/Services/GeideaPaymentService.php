b2a64f6c25de501dfbb8e4fc596be00601a8e460
moofx19 <mahmoudofx@gmail.com>
2025-05-27 14:42:47 +0300

    Update GeideaPaymentService to switch to test credentials for publicKey and apiPassword

--- app/Services/GeideaPaymentService.php
+++ app/Services/GeideaPaymentService.php
@@ -15,10 +15,10 @@ class GeideaPaymentService
     public function __construct()
     {
         $this->baseUrl = 'https://api.merchant.geidea.net/payment-intent/api/v2/direct/session'; // Correct URL for payment session
-        // $this->publicKey = 'c940b85f-c8f7-4229-a853-7c44d4a8db2f'; //test
-        // $this->apiPassword ='225235e9-336a-45aa-91b4-ff9cfd31be50';
-        $this->publicKey = 'b42f3fd5-782e-4d86-afda-0efbed7a1711'; //live
-        $this->apiPassword ='81e08fa6-00eb-4b52-b2fc-89071ebed43d';
+        $this->publicKey = 'c940b85f-c8f7-4229-a853-7c44d4a8db2f'; //test
+        $this->apiPassword ='225235e9-336a-45aa-91b4-ff9cfd31be50';
+        // $this->publicKey = 'b42f3fd5-782e-4d86-afda-0efbed7a1711'; //live
+        // $this->apiPassword ='81e08fa6-00eb-4b52-b2fc-89071ebed43d';
     }
 
     /**
@@ -26,7 +26,8 @@ class GeideaPaymentService
      */
 function generateSignature($merchantPublicKey, $orderAmount, $orderCurrency, $orderMerchantReferenceId, $apiPassword, $timestamp)
 {
-    $apiPassword = '81e08fa6-00eb-4b52-b2fc-89071ebed43d';
+    $apiPassword = '225235e9-336a-45aa-91b4-ff9cfd31be50'; //test
+    $apiPassword = '225235e9-336a-45aa-91b4-ff9cfd31be50'; //live
     // dd($merchantPublicKey, $orderAmount, $orderCurrency, $orderMerchantReferenceId, $apiPassword, $timestamp);
     $amountStr = number_format($orderAmount, 2, '.', '');
     $data = "{$merchantPublicKey}{$amountStr}{$orderCurrency}{$orderMerchantReferenceId}{$timestamp}";
