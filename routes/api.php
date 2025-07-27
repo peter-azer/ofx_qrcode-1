351dd0cd6ff78a38a6da85c03a735bd2503ed477
moofx19 <mahmoudofx@gmail.com>
2025-07-27 10:56:43 +0300

    "fix route typo"

--- routes/api.php
+++ routes/api.php
@@ -27,7 +27,7 @@ use App\Http\Middleware\CheckSEORole;
 
 ###########################################################USER_AUTH########################################################################################
 Route::get('/user_info', [AuthController::class, 'getUserData'])->middleware('auth:sanctum');
-Rouet::put('/user_info/update', [UserController::class, 'update'])->middleware('auth:sanctum');
+Route::put('/user_info/update', [UserController::class, 'update'])->middleware('auth:sanctum');
 
 Route::post('/admin/signup', [AuthController::class, 'Admin_signup']);
 Route::post('/login', [AuthController::class, 'login']);
