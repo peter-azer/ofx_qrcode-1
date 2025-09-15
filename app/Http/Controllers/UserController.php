398a455c11bdea78759371c6385b3f7204db85e7
moofx19 <mahmoudofx@gmail.com>
2025-09-15 16:15:34 +0300

    test

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -32,6 +32,7 @@ class UserController extends Controller
 
         public function destroy(Request $request, $id){
         try {
+            dd($id);
             if(auth()->user()->id == $id){
                 return response()->json(['message' => 'You cannot delete your own account'], 403);
             }
